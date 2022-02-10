842fe6fbffd8783bc4507db1d8c2fcb8f66e7f49
Obed Laws <obedlaws@outlook.com>
2022-02-10 12:44:56 -0500

    Change uint amount for registerVoters

--- contracts/RiverDAOvoting.sol
+++ contracts/RiverDAOvoting.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
