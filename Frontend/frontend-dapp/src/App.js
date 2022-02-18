b7fbbd1da8a25a0be3ce48acc6f0f1d0f630120a
Obed Laws <obedlaws@outlook.com>
2022-02-18 14:15:09 -0500

    ..

---
+++ Frontend/frontend-dapp/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header class="App">
+        RIVER DAO VOTING SESSION #0001
+      </header>
+    </div>
+  );
+}
+
+export default App;
